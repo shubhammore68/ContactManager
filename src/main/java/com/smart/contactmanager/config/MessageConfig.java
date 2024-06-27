94fcb331a5be35e296494d75700c656cf4447296
Shubham More <shubhamdmore03@gmail.com>
2024-06-28 00:01:20 +0530

    stored signup data in database

---
+++ src/main/java/com/smart/contactmanager/config/MessageConfig.java
@@ -0,0 +1,37 @@
+package com.smart.contactmanager.config;
+
+
+
+
+public class MessageConfig {
+    private String message;
+    private String type;
+    
+    public MessageConfig() {
+    }
+    
+    public MessageConfig(String message, String type) {
+        this.message = message;
+        this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    
+
+    
+}
--- src/main/java/com/smart/contactmanager/controller/HomeController.java
+++ src/main/java/com/smart/contactmanager/controller/HomeController.java
@@ -4,18 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.smart.contactmanager.Entities.User;
 import com.smart.contactmanager.Repository.UserRepository;
+import com.smart.contactmanager.config.MessageConfig;
+
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
 public class HomeController {
     
-    // @Autowired
-    // private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/")
     public String home(Model model){
@@ -34,7 +41,37 @@ public class HomeController {
     @GetMapping("/signup")
     public String signup(Model model) {
         model.addAttribute("title", "Register - Smart Contact Manager");
+        model.addAttribute("user", new User());
         return "signup";
     }
+
+    @PostMapping("/do_register")
+    public String postMethodName(@ModelAttribute("user") User user, @RequestParam(value = "aggrement", defaultValue= "false") boolean aggrement, HttpSession session) {
+
+
+        try {
+            if (!aggrement) {
+                System.out.println("you have not agreed term and conditions");
+            }
+    
+            user.setRole("ROLE_USER");
+            user.setEnabled(true);
+            
+            User result =  this.userRepository.save(user);
+            session.setAttribute("message", "Successfully Registered!!");
+            session.setAttribute("type", "alert-success");
+            // session.setAttribute("message", new MessageConfig("User created successfully", "alert-success"));
+
+            return "signup";
+        } catch (Exception e) {
+           e.printStackTrace();
+            session.setAttribute("message", "something went wrong");
+            session.setAttribute("type", "alert-danger");
+            // session.setAttribute("message", new MessageConfig("something went wrong", "alert-danger"));
+            return "signup";
+        }
+
+    }
+    
     
 }
